44362e16f1422fff320b231c0835f20e431b588a
renba <cs_abner@hotmail.com>
2016-12-12 08:16:12 -0600

    controller new_user done

--- app/controllers/new_user.php
+++ app/controllers/new_user.php
@@ -2,3 +2,11 @@
 $matricula = $_POST["matricula"];
 $name = $_POST["name"];
 $email = $_POST["email"];
+
+require_once("../daos/userDao.php");
+
+if(saveInfo($matricula, $name, $email)){
+  header('location: ../views/index.php');
+}else{
+  header('location: ../views/new_user.php');  
+}
