2145e80d34beea0e769493593dd86d0e36130833
renba <cs_abner@hotmail.com>
2016-12-12 12:20:11 -0600

    ssh file changed

--- app/controllers/ssh.php
+++ app/controllers/ssh.php
@@ -2,7 +2,9 @@
 $TargetIP='192.168.69.18';
 $TargetUser='rys2016';
 $TargetPass='2016RyS';
-
+//c4:9a:02:46:a2:64
+//config macfilter add c4:9a:02:46:a2:64 8
+//config macfilter delete c4:9a:02:46:a2:64 
 $MySSH=ssh2_connect($TargetIP);
 ssh2_auth_none($MySSH,$TargetUser);
 $MyShell=ssh2_shell($MySSH);
@@ -11,7 +13,7 @@ fwrite($MyShell,$TargetUser."\r");
 sleep(1);
 fwrite($MyShell,$TargetPass."\r");
 sleep(1);
-fwrite($MyShell,"show serial"."\r");
+fwrite($MyShell,"config macfilter add c4:9a:02:46:a2:64 8"."\r");
 sleep(1);
 while($buffer=fgets($MyShell,4096))
 {
